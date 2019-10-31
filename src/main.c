8b9454e80138c400f4b4271c7caa09545a5938de
matu <matu.0@hotmail.com>
2019-10-31 19:01:34 -0300

    imges

--- src/main.c
+++ src/main.c
@@ -80,7 +80,7 @@
 #define SCT_4 (4) // Panel de control de Generador de funciones
 
 /* Select a compilation choise	*/
-#define TEST (SCT_3)
+#define TEST (SCT_1)
 
 
 #define TICKRATE_1MS	(1)				/* 1000 ticks per second */
